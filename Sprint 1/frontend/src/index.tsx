d8ddc25a059cf4840ba0dbfac449d704be803645
Colin Leary <colinleary123@gmail.com>
2025-10-27 08:40:32 -0400

    copy frontend folder

--- Sprint 1/frontend/src/index.tsx
+++ Sprint 1/frontend/src/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // 1. Import this
@@ -7,11 +6,6 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 import { AuthProvider } from "./context/AuthContext";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
->>>>>>> 8916393b8e09f589da1c0a034f7fa8aaa795708f
 
 root.render(
   <React.StrictMode>
